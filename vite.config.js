1366f1575aa2f3e320ffabc21536498112ec441f
KuoSam <z0935323866@gmail.com>
2025-04-27 18:27:09 +1200

    Update the package.json file to set the project type to module, update the package-lock.json file to specify the Node.js version requirement of 18.0.0, and simplify the build configuration of the vite.config.js file.更新 package.json 文件以将项目类型设置为模块，更新 package-lock.json 文件以指定 Node.js 版本要求为 18.0.0，并简化 vite.config.js 文件的构建配置。

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   base: './',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
+    outDir: 'dist'
   }
 }); 
